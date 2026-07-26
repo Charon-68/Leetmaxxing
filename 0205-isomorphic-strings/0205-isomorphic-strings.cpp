8443b3229e01c416c203deb682984af87e7a50c3
Shourya Saxena <shouryasaxena002@gmail.com>
2026-07-26 13:38:57 +0530

    Time: 3 ms (41.13%), Space: 12 MB (7.72%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        vector<vector<int>> a(128);
+        for(int i=0 ; i<s.length() ; i++){
+            a[(unsigned char)s[i]].push_back(i);
+        }
+        sort(a.begin(), a.end());
+
+        vector<vector<int>> b(128);
+        for(int i=0 ; i<t.length() ; i++){
+            b[(unsigned char)t[i]].push_back(i);
+        }
+        sort(b.begin(), b.end());
+
+        return a == b ? true : false;
+    }
+};
