f3f30dcc3317fb435497601ff059a165f2fc312b
Shourya Saxena <shouryasaxena002@gmail.com>
2026-07-29 12:59:09 +0530

    Time: 0 ms (100%), Space: 9.3 MB (41.22%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        vector<string> ans;
-        int i = 0;
         int n = nums.size();
-        for (int i = 0; i < n; i++){
-            int start = nums[i];
-            while(i + 1 < n && nums[i] + 1 == nums[i + 1]) i++;
-            if (start == nums[i]) ans.push_back(to_string(start));
-            else ans.push_back(to_string(start) + "->" + to_string(nums[i]));
+        vector<string> ans;
+        for(int i=0 ; i<n ; i++){
+            int start = i;
+            while(i+1<n && nums[i+1]  == nums[i] + 1) i++;
+            if(start == i) ans.push_back(to_string(nums[i]));
+            else ans.push_back(to_string(nums[start]) + "->" + to_string(nums[i]));
         }
         return ans;
     }
