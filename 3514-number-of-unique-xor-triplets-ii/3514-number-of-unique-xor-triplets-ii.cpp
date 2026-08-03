d8cb467eebff6b0492f9f4bf6eca54140e33611a
Shourya Saxena <shouryasaxena002@gmail.com>
2026-08-03 18:05:46 +0530

    Time: 599 ms (60.2%), Space: 35.3 MB (80.75%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -2,19 +2,27 @@ class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
-        unordered_set<int> s;
+        vector<bool> two(2048,false);
         
         for(int i=0 ; i<n ; i++){
             for(int j=i ; j<n ; j++){
-                s.insert(nums[i] ^ nums[j]);
+                two[nums[i] ^ nums[j]] = true;
             }
         }
-        unordered_set<int> ans;
-        for(int i=0 ; i<n ; i++){
-            for(int x : s){
-                ans.insert(x ^ nums[i]);
+
+        int ans = 0;
+        vector<bool> three(2048,false);
+
+        for(int i=0 ; i<2048 ; i++){
+            if(!two[i]) continue;
+            for(int j=0 ; j<n ; j++){
+                if(!three[i ^ nums[j]]){
+                    three[i ^ nums[j]] = true;
+                    ans++;
+                }
             }
         }
-        return ans.size();
+
+        return ans;
     }
 };
