17833cf084687464984ee6de220ea6008958d1ac
Shourya Saxena <shouryasaxena002@gmail.com>
2026-08-04 17:52:02 +0530

    Time: 247 ms (5.18%), Space: 228.3 MB (5.1%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<vector<int>> ans;
-        set<pair<int,int>> s;
-        s.insert({0,0});
-
-        priority_queue<vector<int>,vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({nums1[0]+nums2[0],0,0});
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        for(int i = 0; i < nums1.size(); i++) {
+            pq.push({nums1[i] + nums2[0], i, 0});
+        }
 
         while(k--){
             auto curr = pq.top();
@@ -14,16 +13,10 @@ public:
             
             ans.push_back({nums1[curr[1]], nums2[curr[2]]});
 
-            if(curr[1]+1 < nums1.size() && s.find({curr[1]+1,curr[2]}) == s.end()){
-                pq.push({nums1[curr[1]+1] + nums2[curr[2]],curr[1]+1, curr[2]});
-                s.insert({curr[1]+1,curr[2]});
-            }
-            if(curr[2]+1 < nums2.size() && s.find({curr[1],curr[2]+1}) == s.end()){
-                pq.push({nums1[curr[1]] + nums2[curr[2]+1],curr[1], curr[2]+1});
-                s.insert({curr[1],curr[2]+1});
+            if(curr[2] + 1 < nums2.size()){
+                pq.push({nums1[curr[1]] + nums2[curr[2] + 1], curr[1], curr[2] + 1});
             }
         }
         return ans;
     }
 };
-
