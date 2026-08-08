e592e9ff23fe3fdc5e643de13fe5c75102e182b5
Shourya Saxena <shouryasaxena002@gmail.com>
2026-08-09 04:32:00 +0530

    Time: 3 ms (36.52%), Space: 30.5 MB (52.41%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -4,17 +4,15 @@ public:
         int n = nums.size();
         int start = 0;
         int least = 1e6 + 1;
-        for(int i=n-1 ; i>=0 ; i--){
-            if(nums[i] > least) start = i;
-            least = min(least, nums[i]);
-        }
         int end = -1;
         int greatest = INT_MIN;
         for(int i=0 ; i<n ; i++){
+            if(nums[n-1-i] > least) start = n-1-i;
+            least = min(least, nums[n-1-i]);
+
             if(nums[i] < greatest) end = i;
             greatest = max(greatest, nums[i]);
         }
-
         return end - start + 1;
     }
 };
